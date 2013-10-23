4c6b66bee4c332017da158a1b635ba055cf9a4d0
Marek Lichtner <marek@licht.sk>
2013-10-23 16:06:52 +0200

    Possible add more than one same pizza

--- app/js/list/Cart.js
+++ app/js/list/Cart.js
@@ -5,19 +5,31 @@ app.service('Cart', function ($rootScope) {
 
 	self.total = 0;
 	self.cart = [];
-	
+
 	self.add = function (pizza) {
-		self.cart.push(pizza);
+		var newPizza = true;
+		for (var i = 0; i < self.cart.length; i++) {
+			if (self.cart[i].pizza.id == pizza.id) {
+				self.cart[i].count += 1;
+				newPizza = false;
+			}
+		}
+		if (newPizza) {
+			self.cart.push({
+				"count": 1,
+				"pizza": pizza
+			});
+		}
 		self.total += pizza.price;
 	};
 
 	self.clear = function () {
-		self.cart = [];
 		self.total = 0;
+		self.cart = [];
 	};
 
 	createPersistentProperty('cart', 'fmCart', Array);
-    createPersistentProperty('total', 'fmTotal', Number);
+	createPersistentProperty('total', 'fmTotal', Number);
 
 	function createPersistentProperty(localName, storageName, Type) {
 		var json = localStorage[storageName];
@@ -25,11 +37,11 @@ app.service('Cart', function ($rootScope) {
 		self[localName] = json ? JSON.parse(json) : new Type;
 
 		$rootScope.$watch(
-			function() { return self[localName]; },
-			function(value) {
-				if (value) {
-					localStorage[storageName] = JSON.stringify(value);
-				}
+			function () {
+				return self[localName];
+			},
+			function (value) {
+				localStorage[storageName] = JSON.stringify(value);
 			},
 			true
 		);
